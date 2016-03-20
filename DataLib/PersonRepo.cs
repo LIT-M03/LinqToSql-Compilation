5c0c31c274ef7e99dc48f65083cbf0d0560689ef
Avrumi Friedman <info@lakewoodprogramming.com>
2016-03-20 09:26:58 -0400

    Initial Commit

---
+++ DataLib/PersonRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLib
+{
+    public class PersonRepo
+    {
+        private readonly string _connectionString;
+
+        public PersonRepo(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<People> GetPeople()
+        {
+            using (var context = new PersonCarsDataContext(_connectionString))
+            {
+                var loadOptions = new DataLoadOptions();
+                loadOptions.LoadWith<People>(p => p.Cars);
+                context.LoadOptions = loadOptions;
+                return context.Peoples.ToList();
+            }
+        } 
+    }
+}
---
+++ MvcApplication18/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLib;
+
+namespace MvcApplication18.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var repo = new PersonRepo(Properties.Settings.Default.ConStr);
+            return View(new HomePageViewModel{People = repo.GetPeople()});
+        }
+
+    }
+
+    public class HomePageViewModel
+    {
+        public IEnumerable<People> People { get; set; } 
+    }
+}
